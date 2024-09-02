55dbfc34e68e3b317b39288ed27e7c71f7d3fe2e
FaiyazAhmed18 <01.faiyazahmed@gmail.com>
2024-09-02 13:39:49 -0700

    initial project commit

---
+++ Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StataIT.Data;
+using StataIT.Models;
+
+namespace StataIT.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+
+    public class ProductTypesController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            //var data=_db.ProductTypes.ToList()
+            return View(_db.ProductTypes.ToList());
+        }
+
+        //Create Get action Method
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Create Post action Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.ProductTypes.Add(productTypes);
+                await _db.SaveChangesAsync();
+                TempData["save"] = "Product type Saved Successfully";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productTypes);
+        }
+
+        //Get Edit action Method
+        public ActionResult Edit(int? id)
+        {
+            if (id==null)
+            {
+                return NotFound();
+
+            }
+            var productType = _db.ProductTypes.Find(id);
+            if(productType==null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //Post Edit action Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(productTypes);
+                await _db.SaveChangesAsync();
+                TempData["save"] = "Product type Edited Successfully";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productTypes);
+        }
+
+        //Get Details action Method
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+
+            }
+            var productType = _db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //Post Details action Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details(ProductTypes productTypes)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        //Get Delete action Method
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+
+            }
+            var productType = _db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //Post Delete action Method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id, ProductTypes productTypes)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+
+            if (id!=productTypes.ID)  /*if the route value and product value is NotFound same*/
+            {
+                return NotFound();
+            }
+
+            var productType = _db.ProductTypes.Find(id); 
+            if(productType==null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Remove(productType);
+                await _db.SaveChangesAsync();
+                TempData["save"] = "Product type Deleted Successfully";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productTypes);
+        }
+    }
+}
---
+++ Areas/Customer/Controllers/UserController.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StataIT.Models;
+using StataIT.Data;
+
+namespace StataIT.Areas.Customer
+{
+    [Area("Customer")]
+    public class UserController : Controller
+    {
+        UserManager<IdentityUser> _userManager;
+        ApplicationDbContext _db;
+        public UserController(UserManager<IdentityUser>userManager,ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;  
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.ApplicationUsers.ToList());
+        }
+        
+        //Get method for create 
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        //Post method for create
+        [HttpPost]
+        public async Task<IActionResult> Create(ApplicationUser user)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userManager.CreateAsync(user, user.PasswordHash);
+                if (result.Succeeded)
+                {
+                    var isSaveRole = await _userManager.AddToRoleAsync(user, "User");
+                    TempData["save"] = "User has been created successfully";
+                    return RedirectToAction(nameof(Index));
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+
+            return View();
+        }
+
+        //Edit Get method
+        public async Task<IActionResult>Edit(string id)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(c => c.Id == id);
+            if(user==null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        //Post method of edit
+        [HttpPost]
+        public async Task<IActionResult> Edit(ApplicationUser user)
+        {
+            var userInfo = _db.ApplicationUsers.FirstOrDefault(c => c.Id == user.Id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+            userInfo.FirstName = user.FirstName;
+            userInfo.LastName = user.LastName;
+            var result =await _userManager.UpdateAsync(userInfo);
+            if (result.Succeeded)
+            {
+                TempData["Save"] = "User Updated Successfuly";
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        //Details Get method
+        public async Task<IActionResult> Details(string id)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(c => c.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // LockOut/delete get action method
+        public async Task<IActionResult>Lockout(string id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            var user = _db.ApplicationUsers.FirstOrDefault(c => c.Id == id);
+            if(user==null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        //Post action Lockout method
+        [HttpPost]
+        public async Task<IActionResult>Lockout(ApplicationUser user)
+        {
+            var userInfo = _db.ApplicationUsers.FirstOrDefault(c => c.Id == user.Id);
+            if(userInfo==null)
+            {
+                return NotFound();
+            }
+            userInfo.LockoutEnd = DateTime.Now.AddYears(100);
+            int rowAffected=_db.SaveChanges();
+            if(rowAffected>0)
+            {
+                TempData["Save"] = "User Locked out Successfuly";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userInfo);
+        }
+
+        //Get function to Un-Lockout a user
+        public async Task<IActionResult> Active(string id)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(c => c.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        //Post action to unlock a user
+        [HttpPost]
+        public async Task<IActionResult> Active(ApplicationUser user)
+        {
+            var userInfo = _db.ApplicationUsers.FirstOrDefault(c => c.Id == user.Id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+            userInfo.LockoutEnd = DateTime.Now.AddDays(-1);
+            int rowAffected = _db.SaveChanges();
+            if (rowAffected > 0)
+            {
+                TempData["save"] = "User activated Successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userInfo);
+        }
+
+        //Get function to delete a user
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = _db.ApplicationUsers.FirstOrDefault(c => c.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        //Post action to delete a user
+        [HttpPost]
+        public async Task<IActionResult> Delete(ApplicationUser user)
+        {
+            var userInfo = _db.ApplicationUsers.FirstOrDefault(c => c.Id == user.Id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+            _db.ApplicationUsers.Remove(userInfo);
+            int rowAffected = _db.SaveChanges();
+            if (rowAffected > 0)
+            {
+                TempData["save"] = "User deleted Successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userInfo);
+        }
+
+    }
+}
